4800fe95180fd15e58c77ec5cb7b2a6c8b2ee044
William Jones <37749088+wdj82@users.noreply.github.com>
2021-11-28 17:17:46 -0600

    added key text

--- app/components/Key.jsx
+++ app/components/Key.jsx
@@ -2,7 +2,7 @@ import useKeyPress from '~/hooks/useKeyPress';
 
 function Key({ keyCode }) {
     const isKeyPressed = useKeyPress(keyCode);
-    return <div className={isKeyPressed ? 'col played' : 'col'} />;
+    return <div className={isKeyPressed ? 'col played' : 'col'}>{keyCode}</div>;
 }
 
 export default Key;
