0fb8a7ea4c2d88134ef11436c258dc9dcd113625
William Jones <37749088+wdj82@users.noreply.github.com>
2021-11-30 22:01:24 -0600

    pulled logic out of each key into index

--- app/components/Key.jsx
+++ app/components/Key.jsx
@@ -1,57 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
+// import { useEffect, useRef, useState } from 'react';
 
 import useKeyPress from '~/hooks/useKeyPress';
 
 const maxTime = 16;
 
-function useInterval(callback, delay) {
-    const intervalRef = useRef(null);
-    const savedCallback = useRef(callback);
-
-    useEffect(() => {
-        savedCallback.current = callback;
-    }, [callback]);
-
-    useEffect(() => {
-        const tick = () => savedCallback.current();
-        if (typeof delay === 'number') {
-            intervalRef.current = window.setInterval(tick, delay);
-            return () => window.clearInterval(intervalRef.current);
-        }
-    }, [delay]);
-
-    return intervalRef;
-}
-
-export default function Key({ keyCode, isPlaying, note }) {
+export default function Key({ keyCode, isCurrentNote, value }) {
     const isKeyPressed = useKeyPress(keyCode);
-    const [value, setValue] = useState(0);
-    const [audio] = useState(typeof Audio !== 'undefined' && new Audio(`/audio/${note}.wav`));
 
-    // useInterval(
-    //     () => {
-    //         if (isPlaying) {
-    //             audio.load();
-    //             audio.play();
-    //             setValue(value + 1);
-    //             console.log(value);
-    //         }
-    //     },
-    //     value < maxTime && isPlaying ? 1000 : null,
-    // );
+    // const [audio] = useState(typeof Audio !== 'undefined' && new Audio(`/audio/${note}.wav`));
+
+    // useEffect(() => {
+    //     if (isTargetNote && isKeyPressed && value === 8) {
+    //         hitKey(keyCode);
+    //     }
+    // }, [hitKey, isKeyPressed, isTargetNote, keyCode, value]);
 
     return (
         <>
             <div className={isKeyPressed ? 'col played' : 'col'}>
                 {keyCode}
-                {/* {isPlaying && (
+                {isCurrentNote && (
                     <div
                         className='note'
                         style={{
                             transform: `translateY(${(value / maxTime) * 500}%)`,
                         }}
                     />
-                )} */}
+                )}
             </div>
         </>
     );
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,11 +2,8 @@ import { useState } from 'react';
 
 import stylesURL from '~/styles/index.css';
 import Key from '~/components/Key';
+import { useInterval } from '~/hooks/useInterval';
 
-// pick the note to play at random for now
-// note they need to play also random
-// root note plays for four ticks
-// new note plays for four ticks
 // 9th tick it checks if the correct and only the correct key is being pressed
 // 10, 11, 12, 13, 14, 15, 16 ticks it waits and informs user of result
 // starts over
@@ -14,27 +11,22 @@ import Key from '~/components/Key';
 
 // const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 // const keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']'];
+
+// TODO: simplify the state
 const keysArray = [
-    { keyCode: 'q', isPlaying: false, note: 'C' },
-    { keyCode: 'w', isPlaying: false, note: 'Csharp' },
-    { keyCode: 'e', isPlaying: false, note: 'D' },
-    { keyCode: 'r', isPlaying: false, note: 'Dsharp' },
-    { keyCode: 't', isPlaying: false, note: 'E' },
-    { keyCode: 'y', isPlaying: false, note: 'F' },
-    { keyCode: 'u', isPlaying: false, note: 'Fsharp' },
-    { keyCode: 'i', isPlaying: false, note: 'G' },
-    { keyCode: 'o', isPlaying: false, note: 'Gsharp' },
-    { keyCode: 'p', isPlaying: false, note: 'A' },
-    { keyCode: '[', isPlaying: false, note: 'Asharp' },
-    { keyCode: ']', isPlaying: false, note: 'B' },
+    { keyCode: 'q', isCurrentNote: false, isTargetNote: false, note: 'C' },
+    { keyCode: 'w', isCurrentNote: false, isTargetNote: false, note: 'Csharp' },
+    { keyCode: 'e', isCurrentNote: false, isTargetNote: false, note: 'D' },
+    { keyCode: 'r', isCurrentNote: false, isTargetNote: false, note: 'Dsharp' },
+    { keyCode: 't', isCurrentNote: false, isTargetNote: false, note: 'E' },
+    { keyCode: 'y', isCurrentNote: false, isTargetNote: false, note: 'F' },
+    { keyCode: 'u', isCurrentNote: false, isTargetNote: false, note: 'Fsharp' },
+    { keyCode: 'i', isCurrentNote: false, isTargetNote: false, note: 'G' },
+    { keyCode: 'o', isCurrentNote: false, isTargetNote: false, note: 'Gsharp' },
+    { keyCode: 'p', isCurrentNote: false, isTargetNote: false, note: 'A' },
+    { keyCode: '[', isCurrentNote: false, isTargetNote: false, note: 'Asharp' },
+    { keyCode: ']', isCurrentNote: false, isTargetNote: false, note: 'B' },
 ];
-
-// const KeysContext = createContext();
-
-// function KeysProvider(props) {
-//     const [keyState, setKeyState] = useState();
-//     return <KeysContext.Provider {...props} />;
-// }
 
 export function links() {
     return [{ rel: 'stylesheet', href: stylesURL }];
@@ -44,35 +36,68 @@ function getRandomNumber() {
     return Math.floor(Math.random() * 12);
 }
 
+function getTargetNoteNumber(root) {
+    let number = getRandomNumber();
+    while (number === root) {
+        number = getRandomNumber();
+    }
+    return number;
+}
+
+function createAudio() {
+    if (typeof Audio === 'undefined') return null;
+
+    return keysArray.map(({ note }) => new Audio(`/audio/${note}.wav`));
+}
+
 export default function IndexRoute() {
-    const [keys, setPlayingArray] = useState(keysArray);
+    const [keys, setKeys] = useState(keysArray);
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentNote, setCurrentNote] = useState(() => getRandomNumber());
+    const [targetNote, setTargetNote] = useState(() => getTargetNoteNumber(currentNote));
+    const [value, setValue] = useState(0);
+    const [audioArray] = useState(() => createAudio());
 
     function toggleStart() {
         if (isPlaying) {
-            console.log(`stopping ${keys[currentNote].keyCode}`);
-            const newArray = keys.map((obj) => ({ ...obj }));
-            newArray[currentNote].isPlaying = false;
-            setPlayingArray(newArray);
+            console.log(`stopping`);
+            const newArray = [...keys];
+            newArray[currentNote].isCurrentNote = false;
+            newArray[targetNote].isTargetNote = false;
+            setKeys(newArray);
             setIsPlaying(false);
             setCurrentNote(getRandomNumber());
+            setTargetNote(getTargetNoteNumber(currentNote));
         } else {
             console.log(`starting ${keys[currentNote].keyCode}`);
-            const newArray = keys.map((obj) => ({ ...obj }));
-            newArray[currentNote].isPlaying = true;
-            setPlayingArray(newArray);
+            console.log(`target key is ${keys[targetNote].keyCode}`);
+            const newArray = [...keys];
+            newArray[currentNote].isCurrentNote = true;
+            newArray[targetNote].isTargetNote = true;
+            setKeys(newArray);
             setIsPlaying(true);
         }
     }
 
-    // function stopPlaying(keyCode) {
-    //     console.log(`stopping ${keyCode}`);
-    //     const newPlaying = { ...playingArray };
-    //     newPlaying[keyCode].isPlaying = false;
-    //     setPlayingArray(newPlaying);
-    //     setIsPlaying(false);
-    // }
+    useInterval(
+        () => {
+            if (isPlaying) {
+                if (value < 4) {
+                    audioArray[currentNote].load();
+                    audioArray[currentNote].play();
+                } else if (value >= 4 && value < 8) {
+                    audioArray[targetNote].load();
+                    audioArray[targetNote].play();
+                }
+                setValue(value + 1);
+            }
+        },
+        value < 16 && isPlaying ? 1000 : null,
+    );
+
+    function hitKey(keyCode) {
+        console.log(`hit target key ${keyCode}`);
+    }
 
     return (
         <>
@@ -82,8 +107,8 @@ export default function IndexRoute() {
                 </button>
             </div>
             <div className='wrapper'>
-                {keys.map(({ keyCode, isPlaying, note }) => (
-                    <Key key={keyCode} keyCode={keyCode} isPlaying={isPlaying} note={note} />
+                {keys.map((keys) => (
+                    <Key key={keys.keyCode} hitKey={hitKey} value={value} {...keys} />
                 ))}
             </div>
         </>
