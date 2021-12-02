c6ec29f0aef4bfa205cb028bbdeee11b9eea00a0
William Jones <37749088+wdj82@users.noreply.github.com>
2021-12-01 23:17:54 -0600

    can play notes after the check

--- app/components/Key.jsx
+++ app/components/Key.jsx
@@ -7,19 +7,25 @@ const maxTime = 16;
 export default function Key({ keyCode, isCurrentNote, isTargetNote, value, setCorrect, note }) {
     const isKeyPressed = useKeyPress(keyCode);
     const [className, setClassName] = useState('col');
+    const [audio] = useState(typeof Audio !== 'undefined' && new Audio(`/audio/${note.replace('#', 'sharp')}.wav`));
 
     useEffect(() => {
-        if (isTargetNote && value > 9) {
-            setClassName('col target');
-        } else if (isKeyPressed) {
+        if (isKeyPressed) {
             setClassName('col played');
             if (value === 9 && isTargetNote) {
                 setCorrect(true);
             }
+            if (value > 9) {
+                audio.load();
+                audio.play();
+            }
         } else {
             setClassName('col');
         }
-    }, [setCorrect, isKeyPressed, isTargetNote, keyCode, value]);
+        if (isTargetNote && value > 9) {
+            setClassName('col target');
+        }
+    }, [setCorrect, isKeyPressed, isTargetNote, keyCode, value, audio]);
 
     return (
         <div className={className}>
